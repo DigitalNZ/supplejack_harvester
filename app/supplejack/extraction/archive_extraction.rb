3797c8a231d455c587343321ee904f09f75b51c2
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-06-26 17:01:46 +0200

    style: code_climate fixes

--- app/supplejack/extraction/archive_extraction.rb
+++ app/supplejack/extraction/archive_extraction.rb
@@ -6,8 +6,8 @@ module Extraction
       super()
       @request = request
       @extraction_folder = extraction_folder
-      @extraction_definition = request.extraction_definition
       @response = response
+      @document = nil
     end
 
     def download_archive
@@ -16,8 +16,8 @@ module Extraction
 
     def save_entries(extraction_folder)
       each_entry(extraction_folder) do |_, name|
-        @current_document = build_doc("#{extraction_folder}/#{name}")
-        save
+        current_document = build_doc("#{extraction_folder}/#{name}")
+        save(current_document)
         next_page
       end
     ensure
@@ -27,11 +27,8 @@ module Extraction
     private
 
     def each_entry(extraction_folder)
-      @top_level_entries = []
       Minitar::Input.open(StringIO.new(@document.body)) do |input|
         input.each do |entry|
-          @top_level_entries << entry.full_name if top_level_entry?(entry.full_name)
-
           input.extract_entry(extraction_folder, entry) do |action, name, stats|
             next if action != :file_done
 
@@ -41,15 +38,15 @@ module Extraction
       end
     end
 
-    def top_level_entry?(full_name)
-      return true if full_name.count('/').zero?
-      return true if full_name.count('/') == 1 && full_name[-1] == '/'
-
-      false
-    end
-
     def clear_extracted_archive(extraction_folder)
-      FileUtils.rm_rf(@top_level_entries.compact_blank.map { |file| "#{extraction_folder}/#{file}" })
+      top_level_entries = []
+      Minitar::Input.open(StringIO.new(@document.body)) do |input|
+        input.each do |entry|
+          full_name = entry.full_name
+          top_level_entries << full_name if Archive.top_level_entry?(full_name)
+        end
+      end
+      FileUtils.rm_rf(top_level_entries.compact_blank.map { |file| "#{extraction_folder}/#{file}" })
     end
 
     def build_doc(entry_path)
@@ -64,39 +61,35 @@ module Extraction
       )
     end
 
-    def next_page
-      @extraction_definition.page += 1
+    def extraction_definition
+      @request.extraction_definition
     end
 
-    def save
+    def next_page
+      extraction_definition.page += 1
+    end
+
+    def save(doc)
       raise ArgumentError, 'extraction_folder was not provided in #new' if @extraction_folder.blank?
       raise '#extract must be called before #save AbstractExtraction' if @document.blank?
 
-      @current_document.save(file_path)
+      doc.save(file_path)
     end
 
     def body(extracted_file_path)
-      return extract_file_from_gz(extracted_file_path) if gzipped?(extracted_file_path)
+      body = if Archive.gzipped?(extracted_file_path)
+               Archive.extract_from_gz(extracted_file_path)
+             else
+               File.read(extracted_file_path)
+             end
 
-      File.read(extracted_file_path)
-    end
-
-    def gzipped?(gz_file_path)
-      File.open(gz_file_path, 'rb') do |file|
-        magic_number = file.read(2).unpack('C2')
-        return magic_number == [0x1F, 0x8B]
-      end
-    end
-
-    def extract_file_from_gz(gz_path)
-      body = Zlib::GzipReader.open(gz_path, &:read)
-      File.delete(gz_path)
+      File.delete(extracted_file_path)
       body
     end
 
     def file_path
-      page_str = format('%09d', @extraction_definition.page)[-9..]
-      name_str = @extraction_definition.name.parameterize(separator: '_')
+      page_str = format('%09d', extraction_definition.page)[-9..]
+      name_str = extraction_definition.name.parameterize(separator: '_')
       "#{@extraction_folder}/#{name_str}__-__#{page_str}.json"
     end
 
