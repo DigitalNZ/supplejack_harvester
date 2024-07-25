99dae8bb5dee56d72c1ecb16fbb9d3d9cf8be8a3
Daniela Lemow <danielalemow@Danielas-MBP.boost.co.nz>
2024-07-25 15:50:43 +1200

    fix: Fix report view issues

--- app/models/harvest_report.rb
+++ app/models/harvest_report.rb
@@ -97,7 +97,7 @@ class HarvestReport < ApplicationRecord
   private
 
   def considered_cancelled?
-    statuses.any?('cancelled') || harvest_job.cancelled? || pipeline_job.cancelled?
+    statuses.any?('cancelled') || harvest_job&.cancelled? || pipeline_job.cancelled?
   end
 
   def considered_running?
