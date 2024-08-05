8cdebd54a5fc68e619aedc3be3103dfde50699be
Daniela Lemow <danielalemow@Danielas-MBP.boost.co.nz>
2024-08-05 15:05:17 +1200

    feat: Add boolean field to turn concurrent enrichments on

---
+++ db/migrate/20240805030116_add_run_enrichment_concurrently_to_pipeline_job.rb
@@ -0,0 +1,5 @@
+class AddRunEnrichmentConcurrentlyToPipelineJob < ActiveRecord::Migration[7.1]
+  def change
+    add_column :pipeline_jobs, :run_enrichment_concurrently, :boolean, default: false, null: false
+  end
+end
