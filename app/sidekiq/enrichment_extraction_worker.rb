c4131cea6d8c291463b327e13b6cd7b22fdd2c0c
Daniela Lemow <danielalemow@Danielas-MBP.boost.co.nz>
2024-08-05 14:30:28 +1200

    refactor: Move process enrichment extraction logic into helper class

--- app/sidekiq/enrichment_extraction_worker.rb
+++ app/sidekiq/enrichment_extraction_worker.rb
@@ -5,20 +5,6 @@ class EnrichmentExtractionWorker
   include Extraction::Concerns::EnrichmentExtractionProcess
 
   def perform(extraction_definition_id, extraction_job_id, harvest_job_id, api_record, page)
-    @extraction_definition = ExtractionDefinition.find(extraction_definition_id)
-    @extraction_job = ExtractionJob.find(extraction_job_id)
-    @harvest_job = HarvestJob.find(harvest_job_id)
-
-    enrichment_extraction = Extraction::EnrichmentExtraction.new(@extraction_definition.requests.last, Extraction::ApiRecord.new(api_record), page, @extraction_job.extraction_folder)
-    return unless enrichment_extraction.valid?
-
-    enrichment_extraction.extract_and_save
-    enqueue_record_transformation(@harvest_job, 
-                                  @extraction_definition, 
-                                  api_record, 
-                                  enrichment_extraction.document, 
-                                  page)
-
-    update_harvest_report(@harvest_job.harvest_report)
+    process_enrichment_extraction(extraction_definition_id, extraction_job_id, harvest_job_id, api_record, page)
   end
 end
--- app/supplejack/extraction/concerns/enrichment_extraction_process.rb
+++ app/supplejack/extraction/concerns/enrichment_extraction_process.rb
@@ -4,6 +4,26 @@ module Extraction
       extend ActiveSupport::Concern
 
       included do
+        def process_enrichment_extraction(extraction_definition_id, extraction_job_id, harvest_job_id, api_record, page)
+          extraction_definition = ExtractionDefinition.find(extraction_definition_id)
+          extraction_job = ExtractionJob.find(extraction_job_id)
+          harvest_job = HarvestJob.find(harvest_job_id)
+
+          enrichment_extraction = Extraction::EnrichmentExtraction.new(extraction_definition.requests.last, Extraction::ApiRecord.new(api_record), page, extraction_job.extraction_folder)
+          return unless enrichment_extraction.valid?
+
+          enrichment_extraction.extract_and_save
+          enqueue_record_transformation(harvest_job, 
+                                        extraction_definition, 
+                                        api_record, 
+                                        enrichment_extraction.document, 
+                                        page)
+
+          update_harvest_report(harvest_job.harvest_report)
+        end
+
+        private
+
         def enqueue_record_transformation(harvest_job, extraction_definition, api_record, document, page)
           return unless harvest_job.present? && document.successful?
           return if extraction_definition.extract_text_from_file?
