a307553957bb4ce93457c958645f12f9f140a58f
Daniela Lemow <danielalemow@Danielas-MBP.boost.co.nz>
2024-08-06 10:16:06 +1200

    fix: Rubocop fixes

--- spec/value_objects/extraction_params_spec.rb
+++ spec/value_objects/extraction_params_spec.rb
@@ -3,13 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe ExtractionParams do
-  let(:extraction_definition) { double("ExtractionDefinition") }
-  let(:extraction_job) { double("ExtractionJob") }
-  let(:harvest_job) { double("HarvestJob") }
-  let(:api_record) { double("APIRecord") }
-  let(:page) { double("Page") }
-
   subject { described_class.new(extraction_definition, extraction_job, harvest_job, api_record, page) }
+
+  let(:extraction_definition) { double('ExtractionDefinition') }
+  let(:extraction_job) { double('ExtractionJob') }
+  let(:harvest_job) { double('HarvestJob') }
+  let(:api_record) { double('APIRecord') }
+  let(:page) { double('Page') }
 
   describe '#initialize' do
     it 'assigns extraction_definition' do
