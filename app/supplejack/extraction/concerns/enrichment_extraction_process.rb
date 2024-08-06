ddcbc08c57b1dfa4251bb540a8d8768bf2e672b0
Daniela Lemow <danielalemow@Danielas-MBP.boost.co.nz>
2024-08-07 10:45:34 +1200

    fix: Rubocop fixes

--- app/supplejack/extraction/concerns/enrichment_extraction_process.rb
+++ app/supplejack/extraction/concerns/enrichment_extraction_process.rb
@@ -33,22 +33,16 @@ module Extraction
         extraction_definition_id = parsed_params['extraction_definition_id']
         extraction_job_id = parsed_params['extraction_job_id']
         harvest_job_id = parsed_params['harvest_job_id']
-      
-        extraction_definition = ExtractionDefinition.find(extraction_definition_id)
-        extraction_job = ExtractionJob.find(extraction_job_id)
-        enrichment_extraction = build_enrichment_extraction(parsed_params, extraction_definition, extraction_job)
+
         harvest_job = HarvestJob.find(harvest_job_id) if harvest_job_id.present?
-      
+
         ExtractionContext.new(
-          extraction_definition,
-          extraction_job,
-          enrichment_extraction,
-          harvest_job,
-          parsed_params['api_record'],
-          parsed_params['page']
+          ExtractionDefinition.find(extraction_definition_id),
+          ExtractionJob.find(extraction_job_id),
+          build_enrichment_extraction(parsed_params, extraction_definition, extraction_job),
+          harvest_job, parsed_params['api_record'], parsed_params['page']
         )
       end
-      
 
       def build_enrichment_extraction(parsed_params, extraction_definition, extraction_job)
         Extraction::EnrichmentExtraction.new(
@@ -60,7 +54,7 @@ module Extraction
       end
 
       def handle_harvest_job(extraction_context)
-        return unless extraction_context.harvest_job.present?
+        return if extraction_context.harvest_job.blank?
 
         enqueue_record_transformation(extraction_context)
         update_harvest_report(extraction_context)
@@ -70,21 +64,21 @@ module Extraction
         harvest_job = extraction_context.harvest_job
         enrichment_extraction = extraction_context.enrichment_extraction
         extraction_definition = extraction_context.extraction_definition
-      
+
         return unless harvest_job.present? && enrichment_extraction.document.successful?
         return if extraction_definition.extract_text_from_file?
-      
-        TransformationWorker.perform_async(harvest_job.id, 
-                                           extraction_context.page, 
+
+        TransformationWorker.perform_async(harvest_job.id,
+                                           extraction_context.page,
                                            extraction_context.api_record['id'])
-      
+
         harvest_report = harvest_job.harvest_report
         harvest_report.increment_transformation_workers_queued! if harvest_report.present?
       end
 
       def update_harvest_report(extraction_context)
         harvest_report = extraction_context.harvest_job.harvest_report
-        return unless harvest_report.present?
+        return if harvest_report.blank?
 
         harvest_report.increment_pages_extracted!
         harvest_report.update(extraction_updated_time: Time.zone.now)
--- spec/value_objects/extraction_params_spec.rb
+++ spec/value_objects/extraction_params_spec.rb
@@ -1,13 +1,3 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
-RSpec.describe ExtractionParams do
-  subject { described_class.new(extraction_definition, extraction_job, harvest_job, api_record, page) }
-
-  let(:extraction_definition) { double('ExtractionDefinition') }
-  let(:extraction_job) { double('ExtractionJob') }
-  let(:harvest_job) { double('HarvestJob') }
-  let(:api_record) { double('APIRecord') }
-  let(:page) { double('Page') }
-end
