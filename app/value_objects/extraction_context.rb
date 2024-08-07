e4b6047fb3e84499e96eade254f7c295433bd419
Daniela Lemow <danielalemow@Danielas-MBP.boost.co.nz>
2024-08-07 15:31:56 +1200

    fix: Rubocop fix

--- app/value_objects/extraction_context.rb
+++ app/value_objects/extraction_context.rb
@@ -3,6 +3,7 @@
 class ExtractionContext
   attr_reader :extraction_definition, :extraction_job, :enrichment_extraction, :harvest_job, :api_record, :page
 
+  # rubocop:disable Metrics/ParameterLists
   def initialize(extraction_definition, extraction_job, enrichment_extraction, harvest_job, api_record, page)
     @extraction_definition = extraction_definition
     @extraction_job = extraction_job
@@ -11,4 +12,5 @@ class ExtractionContext
     @api_record = api_record
     @page = page
   end
+  # rubocop:enable Metrics/ParameterLists
 end
