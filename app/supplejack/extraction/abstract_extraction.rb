43c2135d121f11641cb3ed8c8bcfe51172add22a
Richard Matthews <rj.matthews23@gmail.com>
2024-08-22 16:19:00 +1200

    Reset unwanted change

--- app/supplejack/extraction/abstract_extraction.rb
+++ app/supplejack/extraction/abstract_extraction.rb
@@ -6,7 +6,7 @@ module Extraction
 
     def extract
       ::Retriable.retriable do
-        Extraction::Request.new(url:, params:, headers:).send(http_method)
+        @document = Extraction::Request.new(url:, params:, headers:).send(http_method)
       end
     rescue StandardError => e
       ::Sidekiq.logger.info "Extraction error: #{e}" if defined?(Sidekiq)
