845d239930ecbe768552837d5e6f9a76a02e6adf
Richard Matthews <rj.matthews23@gmail.com>
2024-08-26 14:17:32 +1200

    Rubocop

--- app/supplejack/extraction/javascript_request.rb
+++ app/supplejack/extraction/javascript_request.rb
@@ -45,7 +45,7 @@ module Extraction
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_argument('--ignore-certificate-errors')
       options.add_argument('--disable-translate')
-      options.add_argument("--no-sandbox")
+      options.add_argument('--no-sandbox')
       options.add_argument('--headless')
       options.add_argument('--disable-gpu')
       Selenium::WebDriver.for(:chrome, options:)
