57a772b21998bcb685f67314af08b8a852ec9688
Richard Matthews <rj.matthews23@gmail.com>
2024-09-10 11:51:23 +1200

    Rubocop

--- db/migrate/20240909214718_change_parameter_content_column_size.rb
+++ db/migrate/20240909214718_change_parameter_content_column_size.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class ChangeParameterContentColumnSize < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column :parameters, :content, :text
+  end
+
+  def down
+    change_column :parameters, :content, :string
   end
 end
--- db/migrate/20240909234254_change_size_of_base_url.rb
+++ db/migrate/20240909234254_change_size_of_base_url.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class ChangeSizeOfBaseUrl < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column :extraction_definitions, :base_url, :text
+  end
+
+  def down
+    change_column :extraction_definitions, :base_url, :string
   end
 end
