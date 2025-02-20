1053ebf0e36fb69a5b7ac404672641d15f9c9bc6
Richard Matthews <rj.matthews23@gmail.com>
2025-02-20 14:19:27 +1300

    Error handling for creating the pipeline job via an API

--- app/controllers/api/pipeline_jobs_controller.rb
+++ app/controllers/api/pipeline_jobs_controller.rb
@@ -4,20 +4,22 @@ module Api
   class PipelineJobsController < ApplicationController
     skip_before_action :authenticate_user!
     skip_before_action :setup_two_factor_authentication
+    skip_before_action :verify_authenticity_token
 
     def create
       pipeline = Pipeline.find(pipeline_job_params['pipeline_id'])
-      destination = Destination.find(pipeline_job_params['destination_id'])
-
 
       @pipeline_job = PipelineJob.new(
         pipeline_id: pipeline.id, 
         harvest_definitions_to_run: pipeline.harvest_definitions.map(&:id), 
-        destination:
+        destination_id: pipeline_job_params['destination_id']
       )
 
       if @pipeline_job.save
         PipelineWorker.perform_async(@pipeline_job.id)
+        render json:  { status: 'success' }
+      else
+        render json: { status: 'failed' }
       end
     end
 
--- spec/requests/api/pipeline_jobs_controller_spec.rb
+++ spec/requests/api/pipeline_jobs_controller_spec.rb
@@ -8,23 +8,78 @@ RSpec.describe "Api::PipelineJobs", type: :request do
   let!(:harvest_definition) { create(:harvest_definition, pipeline:) }
 
   describe "POST /create" do
-    it "create a pipeline job" do
+    context 'When the job is successful' do
+      it "creates a pipeline job" do
+        expect do
+          post api_pipeline_jobs_path, params: {
+            pipeline_job: {
+              destination_id: destination.id,
+              pipeline_id: pipeline.id
+            }
+          }
+        end.to change(PipelineJob, :count).by(1)
+      end
 
-      post api_pipeline_jobs_path, params: {
-        pipeline_job: {
-          destination_id: destination.id,
-          pipeline_id: pipeline.id
+      it "queues a PipelineWorker" do
+        expect(PipelineWorker).to receive(:perform_async)
+
+        post api_pipeline_jobs_path, params: {
+          pipeline_job: {
+            destination_id: destination.id,   
+            pipeline_id: pipeline.id
+          }
         }
-      }
+      end
+  
+      it "returns that the job is successful" do
+        post api_pipeline_jobs_path, params: {
+          pipeline_job: {
+            destination_id: destination.id,
+            pipeline_id: pipeline.id
+          }
+        }
 
-      # expect do
-      #   post api_pipeline_jobs_path, params: {
-      #     pipeline_job: {
-      #       destination_id: destination.id,
-      #       pipeline_id: pipeline.id
-      #     }
-      #   }
-      # end.to change(PipelineJob, :count).by(1)
+        parsed_response = JSON.parse(response.body)
+
+        expect(parsed_response['status']).to eq('success')
+      end
+    end
+
+    context 'When the job fails' do
+      it "does not create a pipeline job" do
+        expect do
+          post api_pipeline_jobs_path, params: {
+            pipeline_job: {
+              destination_id: nil,
+              pipeline_id: pipeline.id
+            }
+          }
+        end.to change(PipelineJob, :count).by(0)
+      end
+
+      it "returns that the job failed" do
+        post api_pipeline_jobs_path, params: {
+          pipeline_job: {
+            destination_id: nil,
+            pipeline_id: pipeline.id  
+          }
+        }
+
+        parsed_response = JSON.parse(response.body)
+
+        expect(parsed_response['status']).to eq('failed')
+      end
+
+      it "does not queue a PipelineWorker" do
+        expect(PipelineWorker).not_to receive(:perform_async)
+
+        post api_pipeline_jobs_path, params: {
+          pipeline_job: {
+            destination_id: nil,   
+            pipeline_id: pipeline.id
+          }
+        }
+      end
     end
   end
 end
