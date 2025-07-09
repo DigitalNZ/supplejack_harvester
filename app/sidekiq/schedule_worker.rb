d70f2fe997f7e7e66f4482f274cfc3b348ce5741
Richard Matthews <rj.matthews23@gmail.com>
2025-07-10 11:25:45 +1200

    Fix issue that was preventing schedules from being run

--- app/sidekiq/schedule_worker.rb
+++ app/sidekiq/schedule_worker.rb
@@ -26,7 +26,7 @@ class ScheduleWorker
       destination_id: schedule.destination.id,
       key: SecureRandom.hex,
       page_type: :all_available_pages,
-      schedule_id: id, delete_previous_records: schedule.delete_previous_records
+      schedule_id: schedule.id, delete_previous_records: schedule.delete_previous_records
     )
   end
 end
---
+++ spec/sidekiq/schedule_worker_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ScheduleWorker, type: :job do
+  let(:destination) { create(:destination) }
+  let(:pipeline) { create(:pipeline) }
+  let(:harvest_definition) { create(:harvest_definition, pipeline:) }
+  let(:field) { create(:field, name: 'title', block: "JsonPath.new('title').on(record).first",
+                 transformation_definition: harvest_definition.transformation_definition) }
+
+  describe '#perform' do
+    context "when scheduling a Pipeline" do
+      let(:schedule) { create(:schedule, pipeline:, destination:, harvest_definitions_to_run: [harvest_definition.id]) }
+
+      it "create a Pipeline Job" do
+
+        expect { described_class.new.perform(schedule.id) }.to change(PipelineJob, :count).by(1)
+      end
+    end
+
+    context "when scheduling an Automation Template" do
+      let(:automation_template) { create(:automation_template) }
+      let(:schedule) { create(:schedule, automation_template:, destination:) }
+
+      it "create a Automation" do
+        expect(AutomationTemplate).to receive(:find).with(automation_template.id).and_return(automation_template)
+        expect(automation_template).to receive(:run_automation)
+
+        described_class.new.perform(schedule.id)
+      end
+    end
+  end
+end
