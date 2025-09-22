4ddbfc05b694c05c87de45ee1f055ca395137dfe
Cierra Higgins <cierra@boost.co.nz>
2025-09-22 16:50:02 +1200

    refactor: clean up migrations

--- db/migrate/20250918235838_create_job_completion_summary.rb
+++ db/migrate/20250918235838_create_job_completion_summary.rb
@@ -5,18 +5,16 @@ class CreateJobCompletionSummary < ActiveRecord::Migration[7.2]
       create_table :job_completion_summaries do |t|
         t.string :extraction_id, null: false
         t.string :extraction_name, null: false
-        t.string :error_type, null: false
-        t.json :error_details, null: false
-        t.integer :error_count, default: 0
-        t.datetime :first_error_at
-        t.datetime :last_error_at
+        t.integer :completion_type, null: false, default: 0 # 0 = error, 1 = stop_condition
+        t.json :completion_details, null: false
+        t.integer :completion_count, default: 0
+        t.datetime :last_occurred_at
         t.timestamps
       end
       
       add_index :job_completion_summaries, :extraction_id, unique: true
-      add_index :job_completion_summaries, :error_type
-      add_index :job_completion_summaries, :first_error_at
-      add_index :job_completion_summaries, :last_error_at
+      add_index :job_completion_summaries, :completion_type
+      add_index :job_completion_summaries, :last_occurred_at
     else
       puts "Table job_completion_summaries already exists, skipping creation"
     end
