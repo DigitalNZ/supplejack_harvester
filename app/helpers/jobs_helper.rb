8319964abf53c1ab6c28671265a08f162765a622
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-10-24 01:42:25 +0200

    chore: updated code to avoid reek issues

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -19,27 +19,30 @@ module JobsHelper
   end
 
   def job_start_time(job)
-    job.start_time.present? ? job.start_time.to_fs(:light) : '-'
+    job.start_time&.to_fs(:light) || '-'
   end
 
   def job_end_time(job)
-    job.end_time.present? ? job.end_time.to_fs(:light) : '-'
+    job.end_time&.to_fs(:light) || '-'
   end
 
   def job_duration(job, format: :long)
-    return if job&.duration_seconds.nil?
+    seconds = job&.duration_seconds
+    return '' unless seconds
 
     case format
     when :short
-      job_duration_seconds_short(job.duration_seconds)
+      job_duration_seconds_short(seconds)
     when :long
-      job_duration_seconds(job.duration_seconds)
+      job_duration_seconds(seconds)
     else
       raise "Unknown duration format #{format}"
     end
   end
 
   def job_duration_seconds(seconds)
+    return '' unless seconds
+
     ActiveSupport::Duration.build(seconds).inspect
   end
 
@@ -51,9 +54,7 @@ module JobsHelper
     format('%<h>d:%<m>02d:%<s>02d', h: hours, m: minutes, s: seconds)
   end
 
-  def job_badge_classes(report, job)
-    status = report&.status || job.status
-
+  def job_badge_classes(status)
     class_names(
       'badge',
       'bg-primary': status == 'completed',
@@ -62,16 +63,17 @@ module JobsHelper
   end
 
   def job_status_label(report, job)
-    return job.cancelled? ? 'Cancelled' : 'Waiting' if report.nil?
+    return job.cancelled? ? 'Cancelled' : 'Waiting' unless report
 
     report.status.capitalize
   end
 
   def job_launched_by_label(pipeline_job)
+    automation_step = pipeline_job.automation_step
     if pipeline_job.schedule.present?
       'Schedule'
-    elsif pipeline_job.automation_step.present?
-      link_to 'Automation', automation_path(pipeline_job.automation_step.automation)
+    elsif automation_step.present?
+      link_to 'Automation', automation_path(automation_step.automation)
     else
       pipeline_job.launched_by&.username
     end
@@ -86,12 +88,9 @@ module JobsHelper
   end
 
   def job_priority_label(report)
-    if report.nil?
-      ''
-    elsif report.harvest_job.present?
-      report.harvest_job.pipeline_job.job_priority&.presence&.humanize || 'No priority'
-    else
-      'No priority'
-    end
+    return '' unless report
+
+    priority = report.harvest_job&.pipeline_job&.job_priority
+    priority.presence&.humanize || 'No priority'
   end
 end
