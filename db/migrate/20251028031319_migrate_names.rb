f15171ff18097cc92d39f3f7d09f749853763a93
Tim Wright <tim@boost.co.nz>
2025-10-29 08:05:44 +1300

    fixed migration

--- db/migrate/20251028031319_migrate_names.rb
+++ db/migrate/20251028031319_migrate_names.rb
@@ -3,28 +3,28 @@ class MigrateNames < ActiveRecord::Migration[7.2]
     # harvest job
     harvest_jobs = HarvestJob.all
     harvest_jobs.each do |job|
-      job.name = "#{id}_#{harvest_definition.kind}"
+      job.name = "#{job.id}_#{job.harvest_definition.kind}"
       job.save
     end
 
     # harvest definition
     harvest_definitions = HarvestDefinition.all
     harvest_definitions.each do |definition|
-      definition.name = "#{id}_#{kind}"
+      definition.name = "#{definition.id}_#{definition.kind}"
       definition.save
     end
 
     # extraction definition
     extraction_definitions = ExtractionDefinition.all
     extraction_definitions.each do |definition|
-      definition.name = "#{id}_#{kind}-extraction"
+      definition.name = "#{definition.id}_#{definition.kind}-extraction"
       definition.save
     end
 
     # extraction jobs
     extraction_jobs = ExtractionJob.all
     extraction_jobs.each do |job|
-      job.name = "#{id}_#{kind}-extraction"
+      job.name = "#{job.id}_#{job.kind}-extraction"
       job.save
     end
   end
