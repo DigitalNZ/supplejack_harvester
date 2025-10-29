f740a16eafb3f1a6e5f3cbf82c96b554758fc10b
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-10-29 21:44:13 +0100

    feat: reviewed the pipeline job page

--- config/initializers/datetime_formats.rb
+++ config/initializers/datetime_formats.rb
@@ -3,3 +3,4 @@
 Time::DATE_FORMATS[:light] = '%A %e %B %Y | %l:%M %p'
 Time::DATE_FORMATS[:file_format] = '%Y-%m-%d_%H-%M-%S'
 Time::DATE_FORMATS[:verbose] = '%A %e %B %Y at %l:%M %p'
+Time::DATE_FORMATS[:medium] = '%H:%M:%S %d/%m/%y'
