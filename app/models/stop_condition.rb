daeb428cb44b900b5083f03402b7653582a4d4bc
Tim Wright <tim@boost.co.nz>
2026-01-21 08:59:41 +1300

    try approach similar to parameter

--- app/models/stop_condition.rb
+++ app/models/stop_condition.rb
@@ -1,20 +1,39 @@
 # frozen_string_literal: true
 
+require 'ostruct'
+
 class StopCondition < ApplicationRecord
   belongs_to :extraction_definition
 
+  # rubocop:disable Lint/UnusedBlockArgument
+  # rubocop:disable Security/Eval
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Style/OpenStructUse
   def evaluate(document_extraction)
+    block = ->(response) { eval(content) }
+
     document = document_extraction.document
-    context = Context.new(document)
+    body = document.body
+    status = document.status
+    headers =
+      if document.respond_to?(:headers)
+        document.headers
+      elsif document.respond_to?(:response_headers)
+        document.response_headers
+      else
+        {}
+      end
 
-    Airbrake.notify(context)
-
-    context.instance_eval(content)
+    block.call(OpenStruct.new({ document: document, body: body, status: status, headers: headers }))
   rescue StandardError => e
     Airbrake.notify(e)
     false
   end
 
+  # rubocop:enable Lint/UnusedBlockArgument
+  # rubocop:enable Security/Eval
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Style/OpenStructUse
   def to_h
     {
       id:,
